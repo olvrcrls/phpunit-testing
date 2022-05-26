d1002a39348aee89cbb0af3d95f2518d28ddd534
olvrcrls <dev.olivercarlos@gmail.com>
2022-05-26 22:43:11 +0800

    Initial commit.

---
+++ src/TagParser.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+class TagParser
+{
+    public function parse(string $tags) : array 
+    {
+        $tags = preg_split("/ ?[,|] ?/", $tags);
+
+        // Old Version / Legacy Version
+        // return array_map(function ($tag) {
+        //     return trim($tag);
+        // }, $tags);
+
+        // New PHP 
+        // return array_map(fn($tag) => trim($tag), $tags);
+        
+        return $tags;
+    }
+}
---
+++ tests/TagParserTest.php
@@ -0,0 +1,78 @@
+<?php
+namespace Tests;
+
+use App\TagParser;
+use PHPUnit\Framework\TestCase;
+
+class TagParserTest extends TestCase
+{
+    protected TagParser $parser;
+
+    protected function setUp(): void
+    {
+        $this->parser = new TagParser;
+    } // setUp
+
+    /**
+     * @dataProvider tagsProvider
+     */
+    public function test_it_parses_tags($input, $expected)
+    {
+        $result = $this->parser->parse($input);
+
+        $this->assertSame($expected, $result);
+    } // test_it_parses_tags
+
+    public function tagsProvider() 
+    {
+        return [
+            array("personal", array("personal"))
+        ];
+    }
+
+    // personal, money, family
+    public function test_it_parses_a_single_tag()
+    {
+        // PHP unit Jargon 
+        // Given | Arrange
+        // $parser = new TagParser;
+
+        // When | Act
+        $result = $this->parser->parse('personal');
+        $expected = ['personal'];
+
+        // Then | Assert
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_it_parses_a_comma_separated_list_of_tags()
+    {
+        // $parser = new TagParser;
+        $result = $this->parser->parse('personal, money, family');
+        $expected = ['personal', 'money', 'family'];
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_it_parses_a_pipe_separated_list_of_tags()
+    {
+        // $parser = new TagParser;
+        
+        $result = $this->parser->parse('personal | money | family');
+        $expected = ['personal', 'money', 'family'];
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_spaces_are_optional()
+    {
+        // $parser = new TagParser;
+
+        $result = $this->parser->parse('personal,money,family');
+        $expected = ['personal', 'money', 'family'];
+        $this->assertSame($expected, $result);
+
+        $result = $this->parser->parse('personal|money| family');
+        $expected = ['personal', 'money', 'family'];
+        $this->assertSame($expected, $result);
+    }
+}
